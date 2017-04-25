26e256528af6f8577bfa303437e0bec13ca65377
Sebastian <sebastian@best4test.com>
2017-04-25 21:03:16 +0100

    post to qaconsole

--- src/com/qaconsole/demo/LoginTest.java
+++ src/com/qaconsole/demo/LoginTest.java
@@ -5,7 +5,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -17,15 +17,17 @@ public class LoginTest {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		System.setProperty("webdriver.gecko.driver","C:\\tools\\geckodriver.exe");
-		driver = new FirefoxDriver();
+//		System.setProperty("webdriver.gecko.driver","C:\\tools\\geckodriver-v0.15.0.exe");
+//		driver = new FirefoxDriver();
+		System.setProperty("webdriver.chrome.driver", "C:\\tools\\chromedriver_2.29.exe");
+		driver = new ChromeDriver();
         wait = new WebDriverWait(driver, 30);
         driver.get("http://demo.qaconsole.com/");
 
 	}
 
 	@Test
-	public void test() {
+	public void test_ASDF1234() {
 		 driver.findElement(By.name("email")).sendKeys("guest@qaconsole.com");
 		 driver.findElement(By.name("password")).sendKeys("guest2017");         
          driver.findElement(By.cssSelector(".btn-lg")).click();
@@ -35,6 +37,11 @@ public class LoginTest {
 	    		 return webDriver.findElement(By.cssSelector(".panel-danger")) != null;
 	    	 }
 	     });
+	}
+	
+	@Test
+	public void ztest_ASDF0015() {
+		 driver.findElement(By.name("email")).sendKeys("guest@qaconsole.com");
 	}
 	
 	 @AfterClass
